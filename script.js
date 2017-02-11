ba4c94163092f93d0144676ad57ab80cba767dbd
pszklanko <pszklanko@gmail.com>
2017-02-11 23:55:58 +0100

    next fieldset, start of js validation

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 
+function validateForm() {
+  let form = document.forms['basicForm'];
+  if ( form['firstname'].value === '' ) {
+    form['firstname'].appendChild('<div>not valid</div>');
+    form['firstname']
+    return false;
+  }
+  return true;
+}
