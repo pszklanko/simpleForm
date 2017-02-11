648dd426bc7d38ec6dd5ea524401ba9e4ecdf004
pszklanko <pszklanko@gmail.com>
2017-02-12 00:48:32 +0100

    validation experiments

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 
 function validateForm() {
+  let namePattern = new RegExp('([A-Z])\w+');
   let form = document.forms['basicForm'];
-  if ( form['firstname'].value === '' ) {
-    form['firstname'].appendChild('<div>not valid</div>');
-    form['firstname']
+  if (namePattern.test(form['firstname'].value) === false) {
+    form['firstname'].className = 'invalid';
+    return false;
+  }
+  if (namePattern.test(form['lastname'].value) === false) {
+    form['lastname'].className = 'invalid';
     return false;
   }
   return true;
